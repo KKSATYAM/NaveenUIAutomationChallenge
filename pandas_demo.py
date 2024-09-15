0f38b52e8f8a293d8ad7220a2faaf1c876de2399
KKSATYAM <70344131+KKSATYAM@users.noreply.github.com>
2024-09-15 09:29:58 +0000

    pandas_session_3

--- pandas_demo.py
+++ pandas_demo.py
@@ -5,5 +5,16 @@ student_marks=[30,40,60,48,89]
 # s=pd.Series(index=student_list,data=student_marks,name="Student List")
 # print(s)
 
-s=pd.Series(data=dict(zip(student_list,student_marks)))
-print(s)
+# s=pd.Series(data=dict(zip(student_list,student_marks)))
+# print(s)
+# series=pd.Series([i for i in range(0,50)])
+# # print(series.tail(-40))
+
+# print(series.head(15).tail(-10))
+# print("*******************************************************")
+# print(series.head(15).tail(5))
+
+# print("*******************************************************")
+# print(series.tail(-10).head())
+
+
